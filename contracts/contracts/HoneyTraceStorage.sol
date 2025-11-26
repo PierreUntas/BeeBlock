a360daabd3ed7eb3aa8712bb5ea8aec0ec036a5d
PierreAnders <pierre.untas@gmail.com>
2025-11-26 16:41:57 +0100

    add merkle proof

--- contracts/contracts/HoneyTraceStorage.sol
+++ contracts/contracts/HoneyTraceStorage.sol
@@ -3,11 +3,17 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 
 interface IHoneyTokenization {
     function mintHoneyBatch(address producer, uint256 amount, string memory uri) external returns (uint256);
 
     function balanceOf(address account, uint256 id) external view returns (uint256);
+
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) external;
+
+    function tokenProducer(uint256 tokenId) external view returns (address);
 }
 
 contract HoneyTraceStorage is Ownable {
@@ -24,6 +30,9 @@ contract HoneyTraceStorage is Ownable {
         uint id;
         string honeyType;
         string metadata;
+        bytes32 merkleRoot;
+        uint256 totalAmount;
+        uint claimedCount;
     }
 
     struct Comment {
@@ -40,16 +49,22 @@ contract HoneyTraceStorage is Ownable {
 
     IHoneyTokenization public honeyTokenization;
 
+    mapping(uint256 => mapping(bytes32 => bool)) private claimedKeys; // tokenId => keyHash => claimed
+
     event NewAdmin(address indexed newAdmin);
     event AuthorizationProducer(address indexed producer, bool isAuthorized);
     event NewProducer(address indexed producer);
     event NewHoneyBatch(address indexed producer, uint indexed honeyBatchId);
+    event HoneyTokenClaimed(address indexed consumer, uint indexed honeyBatchId, bytes32 keyHash);
     event NewComment(address indexed consumer, uint indexed honeyBatchId, uint8 rating);
 
     error onlyAdminAuthorized();
     error authorizationAlreadyApply();
     error producerNotAuthorized();
     error notAllowedToComment();
+    error invalidMerkleProof();
+    error keyAlreadyClaimed();
+    error noTokenLeft();
 
     function addAdmin(address _newAdmin) external onlyOwner {
         admins[_newAdmin] = true;
@@ -87,10 +102,10 @@ contract HoneyTraceStorage is Ownable {
         emit NewProducer(msg.sender);
     }
 
-    function addHoneyBatch(string memory _honeyType, string memory _metadata) external onlyAuthorizedProducer {
+    function addHoneyBatch(string memory _honeyType, string memory _metadata, uint256 _amount, bytes32 _merkleRoot) external onlyAuthorizedProducer {
         uint tokenId = honeyTokenization.mintHoneyBatch(
             msg.sender,
-            100,
+            _amount,
             _metadata
         );
 
@@ -98,9 +113,31 @@ contract HoneyTraceStorage is Ownable {
         honeyBatch.id = tokenId;
         honeyBatch.honeyType = _honeyType;
         honeyBatch.metadata = _metadata;
+        honeyBatch.merkleRoot = _merkleRoot;
+        honeyBatch.totalAmount = _amount;
 
         emit NewHoneyBatch(msg.sender, tokenId);
     }
+
+    function claimHoneyToken(uint256 _honeyBatchId, string memory _secretKey, bytes32[] memory _merkleProof) external {
+        HoneyBatch storage batch = honeyBatches[_honeyBatchId];
+        require(batch.claimedCount < batch.totalAmount, noTokenLeft());
+
+        bytes32 leaf = keccak256(abi.encodePacked(_secretKey));
+
+        require(!claimedKeys[_honeyBatchId][leaf], keyAlreadyClaimed());
+        require(MerkleProof.verify(_merkleProof, batch.merkleRoot, leaf), invalidMerkleProof());
+
+        claimedKeys[_honeyBatchId][leaf] = true;
+        batch.claimedCount += 1;
+
+        address producer = honeyTokenization.tokenProducer(_honeyBatchId);
+
+        honeyTokenization.safeTransferFrom(producer, msg.sender, _honeyBatchId, 1, "");
+
+        emit HoneyTokenClaimed(msg.sender, _honeyBatchId, leaf);
+    }
+
 
     function addComment(uint _honeyBatchId, uint8 _rating, string memory _metadata) external {
         require(honeyTokenization.balanceOf(msg.sender, _honeyBatchId) > 0, notAllowedToComment());
@@ -120,7 +157,12 @@ contract HoneyTraceStorage is Ownable {
         return honeyBatches[_id];
     }
 
-    function getHoneyBatchComments(uint _honeybatchId) external view returns (Comment[] memory) {
-        return honeyBatchesComments[_honeybatchId];
+    function getHoneyBatchComments(uint _honeyBatchId) external view returns (Comment[] memory) {
+        return honeyBatchesComments[_honeyBatchId];
+    }
+
+    function isKeyClaimed(uint256 _honeyBatchId, string memory _secretKey) external view returns (bool) {
+        bytes32 leaf = keccak256(abi.encodePacked(_secretKey));
+        return claimedKeys[_honeyBatchId][leaf];
     }
 }
