5fbe63ab29cb5aa1a7303479ea94a4d47a4dda87
PierreAnders <pierre.untas@gmail.com>
2025-11-27 12:03:43 +0100

    add beforeEach

--- contracts/test/HoneyTraceStorage.ts
+++ contracts/test/HoneyTraceStorage.ts
@@ -4,23 +4,34 @@ import {network} from "hardhat";
 const {ethers} = await network.connect();
 
 describe("HoneyTraceStorage", function () {
+    let owner: any;
+    let admin: any;
+    let produce: any;
+    let customer: any;
+    let honeyTraceStorage: any;
+    let honeyTokenization: any;
 
     async function deployHoneyTraceStorage() {
         const [owner, admin, produce, customer] = await ethers.getSigners();
-
         const HoneyTokenization = await ethers.getContractFactory("HoneyTokenization");
         const honeyTokenization = await HoneyTokenization.deploy("");
         const honeyTokenizationAddress = honeyTokenization.getAddress();
-
-        const HoneyTraceStorage = await ethers.getContractFactory("HoneyTraceStorage");
-        const honeyTraceStorage = await HoneyTraceStorage.deploy(honeyTokenizationAddress);
-
+        const honeyTraceStorage = await ethers.deployContract("HoneyTraceStorage", [honeyTokenizationAddress]);
         return {owner, admin, produce, customer, honeyTraceStorage, honeyTokenization};
     }
 
+    beforeEach(async function () {
+       const deployment = await deployHoneyTraceStorage();
+       owner = deployment.owner;
+       admin = deployment.admin;
+       produce = deployment.produce;
+       customer = deployment.customer;
+       honeyTraceStorage = deployment.honeyTraceStorage;
+       honeyTokenization = deployment.honeyTokenization;
+    })
+
     describe("Deployment", function () {
         it("Should deploy the HoneyTraceStorage contract", async function () {
-            const {owner, honeyTokenization} = await deployHoneyTraceStorage();
             expect(await honeyTokenization.owner()).to.equal(await owner.getAddress());
         })
     })
