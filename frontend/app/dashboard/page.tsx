08764c7bbab8a3f05877d6a0964d58609905be67
PierreAnders <pierre.untas@gmail.com>
2025-12-02 06:22:10 +0100

    add shared components

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Bienvenue sur BeeBlock</p>
+        </div>
+    )
+}
---
+++ frontend/components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <nav className="footer">
+            All rights reserved &copy; BeeBlock {new Date().getFullYear()}
+        </nav>
+    )
+}
+
+export default Footer;
