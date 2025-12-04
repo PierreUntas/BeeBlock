c627589c5a136e025050942ee2f2f8b48f6ce1cd
PierreAnders <pierre.untas@gmail.com>
2025-12-04 11:18:20 +0100

    fix build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false,
+            net: false,
+            tls: false,
+        };
+
+        // Exclure les fichiers de test
+        config.module.rules.push({
+            test: /\.test\.(js|mjs)$/,
+            loader: 'ignore-loader'
+        });
+
+        return config;
+    },
 };
 
 export default nextConfig;
